a4cbe45742317a47a4ec2dcb0009fa3fc6cbb7e2
linsdfghj <2214112680@qq.com>
2024-06-10 17:47:32 +0800

    重置提交

---
+++ src/pages/Personal/Competition/Competition.js
@@ -0,0 +1,24 @@
+/*
+ * @Author: linsdfghj 2214112680@qq.com
+ * @Date: 2024-02-18 22:13:11
+ * @LastEditors: linsdfghj 2214112680@qq.com
+ * @LastEditTime: 2024-06-04 15:43:57
+ * @FilePath: \my-first-reactd:\桌面\second_code_1\src\pages\Personal\Competition\Competition.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import React from 'react'
+import './Competition.css'
+
+
+const Competition = () => {
+  return (
+    <div className='competition-all'>
+      <div className='competition-down'>
+      </div>
+
+    </div>
+
+  )
+}
+
+export default Competition
---
+++ src/pages/Personal/Project/Project.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Project.css'
+
+
+const Project = () => {
+  return (
+    <div className='project-all'>
+      <div className='project-down'>
+
+      </div>
+
+    </div>
+
+  )
+}
+
+export default Project
